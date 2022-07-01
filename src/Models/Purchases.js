d95f66f36b46fb355d31a46bb3e40acc9ccd28c5
mati <mati@gmail.com>
2022-06-30 23:01:53 -0300

    added changes to purchase model, now its able to populate with user db collection

--- src/Models/Purchases.js
+++ src/Models/Purchases.js
@@ -5,17 +5,38 @@ const PurchaseSchema = new Schema({
   PaymentMethod: {
     type: String,
     trim: true,
+    default: null,
   },
 
   status: {
     type: String,
     lowercase: true,
     trim: true,
+    default: null,
+  },
+  total: {
+    type: Number,
+    default: null,
   },
   transactionId: {
     type: String,
     lowercase: true,
     trim: true,
+    default: null,
+  },
+  items: [
+    {
+      type: Schema.Types.ObjectId,
+      ref: "Item",
+    },
+  ],
+  user: {
+    type: Schema.Types.ObjectId,
+    ref: "User",
+  },
+  createdAt: {
+    type: Date,
+    default: Date.now,
   },
 });
 module.exports = mongoose.model("Purchase", PurchaseSchema);
